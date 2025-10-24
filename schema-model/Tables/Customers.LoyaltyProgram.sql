e4fd6561770f48983e64e15bc66379338ee550d9
MrTyRedgate <110102754+MrTyRedgate@users.noreply.github.com>
2025-10-24 11:17:45 +0100

    Delete schema-model directory

--- schema-model/Tables/Customers.LoyaltyProgram.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [Customers].[LoyaltyProgram]
-(
-[ProgramID] [int] NOT NULL IDENTITY(1, 1),
-[ProgramName] [nvarchar] (50) NOT NULL,
-[PointsMultiplier] [decimal] (3, 2) NULL CONSTRAINT [DF__LoyaltyPr__Point__398D8EEE] DEFAULT ((1.0))
-)
-GO
-ALTER TABLE [Customers].[LoyaltyProgram] ADD CONSTRAINT [PK__LoyaltyP__752560385DA5F6D0] PRIMARY KEY CLUSTERED ([ProgramID])
-GO
